f87bad6c93225495f8c84399ec2785f43818081c
Ben <brbryant2639@gmail.com>
2021-06-10 18:12:36 -0500

    Updated haynes.js with console log

---
+++ torry/haynes.js
@@ -0,0 +1,7 @@
+// TORRY! Open your terminal and run 'node torry/haynes.js' (without the single quotes)
+
+let name = "TORRY HAYNES";
+
+for (let index = 0; index < name.length; index++) {
+  console.log(name[index]);
+}
